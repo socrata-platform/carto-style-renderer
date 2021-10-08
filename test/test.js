c3e049d71f6922e1723cdbcb66925199b629c7cf
Michael Chui <michael.chui@socrata.com>
2021-10-08 14:32:07 -0700

    generate expected test output from the CORRECT input

--- test/test.js
+++ test/test.js
@@ -15,18 +15,18 @@ function collapse(xml) {
 describe('style()', function() {
   it('rendering CSS returns expected', function() {
     var expected = '<?xml version="1.0" encoding="utf-8"?>'
-          + '<!DOCTYPE Map[]>'
-          + '<Map>'
-          + '<Style filter-mode="first" name="main">'
-          + '<Rule>'
-          + '<MarkersSymbolizer fill="#000066" stroke="#000099" stroke-width="1" width="3" />'
-          + '<PolygonSymbolizer fill="#990000" />'
-          + '</Rule>'
-          + '</Style>'
-          + '<Layer name="main">'
-          + '<StyleName><![CDATA[main]]></StyleName>'
-          + '</Layer>'
-          + '</Map>';
+        + '<?xml version="1.0" encoding="utf-8"?>'
+        + '<!DOCTYPE Map[]>'
+        + '<Map>'
+        + '<Style filter-mode="first" name="main">'
+        + '<Rule>'
+        + '<MarkersSymbolizer stroke="#0000cc" width="1" />'
+        + '</Rule>'
+        + '</Style>'
+        + '<Layer name="main">'
+        + '<StyleName><![CDATA[main]]></StyleName>'
+        + '</Layer>'
+        + '</Map>';
 
     var oneline = '#main{marker-line-color:#00C;marker-width:1}';
     var multiline = [
